5fcea4a2b110102c2c6d8fe72edf222bfbb76bb9
AndresfGil <andresfgil98@outlook.com>
2022-12-22 12:48:47 -0500

    set width and heigh of canvas responsive

--- game.js
+++ game.js
@@ -1,6 +1,38 @@
 const canvas = document.getElementById('game')
 const game = canvas.getContext('2d')
 
-function startGame(){
+window.addEventListener('load', startGame)
+ 
+function startGame() {
+    let canvasSize;
+
+    if(window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.75;
+    } else {
+        canvasSize = window.innerHeight * 0.75;
+    }
+
+    canvas.setAttribute('width', canvasSize)
+    canvas.setAttribute('height', canvasSize)
+
+    const elementsSize = (canvasSize / 10) - 1 ;
+
+    console.log({canvasSize, elementsSize});
+
+    game.font = elementsSize + 'px Kalam';
+    game.textAlign = '';
+
+for (let i = 0; i < 10; i++) {
+    game.fillText(emojis['X'], elementsSize * i, elementsSize);
+}
+
     
+
+    // game.fillRect(50,0,100,100)
+    // // game.clearRect(50,50,100,50)
+    // // game.clearRect()
+    // game.font = '25px Kalam'
+    // game.fillStyle = 'red';
+    // game.textAlign = 'center'
+    // game.fillText('Hello', 50, 50);
 }
