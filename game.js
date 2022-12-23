6cb54753c4451580c0638e697045918c63c60178
AndresfGil <andresfgil98@outlook.com>
2022-12-23 10:34:12 -0500

    add canvas responsive

--- game.js
+++ game.js
@@ -1,12 +1,24 @@
 const canvas = document.getElementById('game')
 const game = canvas.getContext('2d')
+let canvasSize;
+let elementsSize;
 
-window.addEventListener('load', startGame)
- 
+window.addEventListener('load', setCanvasSize)
+window.addEventListener('resize', setCanvasSize)
+
 function startGame() {
-    let canvasSize;
+    console.log({ canvasSize, elementsSize });
 
-    if(window.innerHeight > window.innerWidth) {
+    game.font = elementsSize + 'px Kalam';
+    game.textAlign = '';
+
+    for (let i = 0; i < 10; i++) {
+        game.fillText(emojis['X'], elementsSize * i, elementsSize);
+    }
+}
+
+function setCanvasSize() {
+    if (window.innerHeight > window.innerWidth) {
         canvasSize = window.innerWidth * 0.75;
     } else {
         canvasSize = window.innerHeight * 0.75;
@@ -15,24 +27,7 @@ function startGame() {
     canvas.setAttribute('width', canvasSize)
     canvas.setAttribute('height', canvasSize)
 
-    const elementsSize = (canvasSize / 10) - 1 ;
+    elementsSize = (canvasSize / 10) - 1;
 
-    console.log({canvasSize, elementsSize});
-
-    game.font = elementsSize + 'px Kalam';
-    game.textAlign = '';
-
-for (let i = 0; i < 10; i++) {
-    game.fillText(emojis['X'], elementsSize * i, elementsSize);
-}
-
-    
-
-    // game.fillRect(50,0,100,100)
-    // // game.clearRect(50,50,100,50)
-    // // game.clearRect()
-    // game.font = '25px Kalam'
-    // game.fillStyle = 'red';
-    // game.textAlign = 'center'
-    // game.fillText('Hello', 50, 50);
+    startGame();
 }
